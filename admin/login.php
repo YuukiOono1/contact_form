41856c074144d4000f70de53277a4f7cc6001943
YuukiOono1 <>
2020-07-20 18:33:11 +0900

    [create] 管理者ログイン

---
+++ admin/login.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title></title>
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css">
+    <!-- Bootstrap core CSS -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Material Design Bootstrap -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.8.11/css/mdb.min.css" rel="stylesheet">
+</head>
+
+<body>
+
+    <section class="form-elegant">
+
+        <!-- Grid row -->
+        <div class="row mt-4">
+
+            <!-- Grid column -->
+            <div class="mx-auto col-md-9 col-lg-7 col-xl-5">
+
+                <!--Form without header-->
+                <div class="card">
+
+                    <div class="card-body">
+
+                        <!--Header-->
+                        <div class="text-center">
+                            <h3 class="dark-grey-text mb-5"><strong>管理ログイン</strong></h3>
+                        </div>
+
+                        <form method="POST" action="">
+                            <div class="md-form">
+                                <input type="email" id="email" name="email" class="form-control" required value="">
+                                <label for="email">メールアドレス</label>
+                            </div>
+                            <div class="md-form">
+                                <input type="password" id="password" name="password" class="form-control" required value="">
+                                <label for="password">パスワード</label>
+                            </div>
+                            <button type="submit" class="btn default-color btn-block btn-rounded z-depth-1a"><span class="white-text">ログイン</span></button>
+                        </form>
+                    </div>
+
+                </div>
+                <!--/Form without header-->
+
+            </div>
+            <!-- Grid column -->
+
+        </div>
+        <!-- Grid row -->
+    </section>
+
+    <!-- JQuery -->
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <!-- Bootstrap tooltips -->
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.4/umd/popper.min.js"></script>
+    <!-- Bootstrap core JavaScript -->
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/js/bootstrap.min.js"></script>
+    <!-- MDB core JavaScript -->
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.8.11/js/mdb.min.js"></script>
+</body>
+
+</html>
