48889e327c4ecac0b3b73c86659ea153e06bb9c5
YuukiOono1 <64980147+YuukiOono1@users.noreply.github.com>
2020-07-22 10:48:53 +0900

    Delete dbconnect.php

--- dbconnect.php
+++
@@ -1,8 +0,0 @@
-<?php
-// DB接続
-try {
-    $db = new PDO('mysql:dbname=task; host=localhost; port=8889; charset=utf8', 'root', 'root');
-} catch(PDOException $e) {
-    echo 'DB接続エラー: ' . $e->getMessage();
-}
-?>
