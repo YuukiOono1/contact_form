7400dbba3d1eebc2aafbfdc6d6409e04f3e753cc
YuukiOono1 <>
2020-07-22 10:46:42 +0900

    [create] admin sidebar

---
+++ admin/sidebar.php
@@ -0,0 +1,11 @@
+<div class="col-2">
+    <div class="sidebar_fixed">
+        <div class="card mt-4 ml-4">
+            <ul class="list-group">
+                <div class="card-header text-center"><a href="index.php">TOP</a></div>
+                <li class="list-group-item"><a href="index.php">お問い合わせ一覧</a></li>
+                <li class="list-group-item"><a href="logout.php">ログアウトする</a></li>
+            </ul>
+        </div>
+    </div>
+</div>
