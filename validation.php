628111e5ea6f10a7d3bf4805b3d5f86d0e16e626
YuukiOono1 <>
2020-07-27 13:35:51 +0900

    [add] post_code and telephone validation

--- validation.php
+++ validation.php
@@ -1,6 +1,4 @@
 <?php
-// バリデーションはあとでもう少し改善
-
 // 未入力チェック
 if (empty($_POST['last_name'])) {
     $error['last_name'] = 'blank';
@@ -17,11 +15,24 @@ if (empty($_POST['first_name_kana'])) {
 if (empty($_POST['email'])) {
     $error['email'] = 'blank';
 }
-if (empty($_POST['post_code[]'])) {
+
+if (empty($_POST['post_code'][0])) {
     $error['post_code'] = 'blank';
 }
 
-if (empty($_POST['telephone[]'])) {
+if (empty($_POST['post_code'][1])) {
+    $error['post_code'] = 'blank';
+}
+
+if (empty($_POST['telephone'][0])) {
+    $error['telephone'] = 'blank';
+}
+
+if (empty($_POST['telephone'][1])) {
+    $error['telephone'] = 'blank';
+}
+
+if (empty($_POST['telephone'][2])) {
     $error['telephone'] = 'blank';
 }
 
@@ -42,11 +53,47 @@ if (strlen($_POST['last_name_kana']) > 20) {
 if (strlen($_POST['first_name_kana']) > 20) {
     $error['first_name_kana'] = 'length';
 }
-if (strlen($_POST['post_code[]']) > 9) {
+if (strlen($_POST['post_code'][0]) > 5) {
     $error['post_code'] = 'length';
+}
+if (strlen($_POST['post_code'][1]) > 4) {
+    $error['post_code'] = 'length';
+}
+
+if (strlen($_POST['telephone'][0]) > 4) {
+    $error['telephone'] = 'length';
+}
+
+if (strlen($_POST['telephone'][1]) > 4) {
+    $error['telephone'] = 'length';
+}
+
+if (strlen($_POST['telephone'][2]) > 4) {
+    $error['telephone'] = 'length';
 }
 
 if (strlen($_POST['content']) > 255) {
     $error['content'] = 'length';
+}
+
+// 数値チェック
+if (preg_match("/[^0-9]/", $_POST['post_code'][0])) {
+    $error['post_code'] = 'match';
+}
+
+if (preg_match("/[^0-9]/", $_POST['post_code'][1])) {
+    $error['post_code'] = 'match';
+}
+
+if (preg_match("/[^0-9]/", $_POST['telephone'][0])) {
+    $error['telephone'] = 'match';
+}
+
+if (preg_match("/[^0-9]/", $_POST['telephone'][1])) {
+    $error['telephone'] = 'match';
+}
+
+if (preg_match("/[^0-9]/", $_POST['telephone'][2])) {
+    $error['telephone'] = 'match';
 }
 ?>
