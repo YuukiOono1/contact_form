8e209e3eeacaeefc72b035d3f42d0a9682067f66
YuukiOono1 <>
2020-07-28 18:32:48 +0900

    [fix] post_code and telephone

--- validation.php
+++ validation.php
@@ -16,23 +16,23 @@ if (empty($_POST['email'])) {
     $error['email'] = 'blank';
 }
 
-if (empty($_POST['post_code'][0])) {
+if (empty($_POST['post_code_first'])) {
     $error['post_code'] = 'blank';
 }
 
-if (empty($_POST['post_code'][1])) {
+if (empty($_POST['post_code_last'])) {
     $error['post_code'] = 'blank';
 }
 
-if (empty($_POST['telephone'][0])) {
+if (empty($_POST['telephone_first'])) {
     $error['telephone'] = 'blank';
 }
 
-if (empty($_POST['telephone'][1])) {
+if (empty($_POST['telephone_middle'])) {
     $error['telephone'] = 'blank';
 }
 
-if (empty($_POST['telephone'][2])) {
+if (empty($_POST['telephone_last'])) {
     $error['telephone'] = 'blank';
 }
 
@@ -53,22 +53,22 @@ if (strlen($_POST['last_name_kana']) > 20) {
 if (strlen($_POST['first_name_kana']) > 20) {
     $error['first_name_kana'] = 'length';
 }
-if (strlen($_POST['post_code'][0]) > 5) {
+if (strlen($_POST['post_code_first']) > 5) {
     $error['post_code'] = 'length';
 }
-if (strlen($_POST['post_code'][1]) > 4) {
+if (strlen($_POST['post_code_last']) > 4) {
     $error['post_code'] = 'length';
 }
 
-if (strlen($_POST['telephone'][0]) > 4) {
+if (strlen($_POST['telephone_first']) > 4) {
     $error['telephone'] = 'length';
 }
 
-if (strlen($_POST['telephone'][1]) > 4) {
+if (strlen($_POST['telephone_middle']) > 4) {
     $error['telephone'] = 'length';
 }
 
-if (strlen($_POST['telephone'][2]) > 4) {
+if (strlen($_POST['telephone_last']) > 4) {
     $error['telephone'] = 'length';
 }
 
@@ -77,23 +77,23 @@ if (strlen($_POST['content']) > 255) {
 }
 
 // 数値チェック
-if (preg_match("/[^0-9]/", $_POST['post_code'][0])) {
+if (preg_match("/[^0-9]/", $_POST['post_code_first'])) {
     $error['post_code'] = 'match';
 }
 
-if (preg_match("/[^0-9]/", $_POST['post_code'][1])) {
+if (preg_match("/[^0-9]/", $_POST['post_code_last'])) {
     $error['post_code'] = 'match';
 }
 
-if (preg_match("/[^0-9]/", $_POST['telephone'][0])) {
+if (preg_match("/[^0-9]/", $_POST['telephone_first'])) {
     $error['telephone'] = 'match';
 }
 
-if (preg_match("/[^0-9]/", $_POST['telephone'][1])) {
+if (preg_match("/[^0-9]/", $_POST['telephone_middle'])) {
     $error['telephone'] = 'match';
 }
 
-if (preg_match("/[^0-9]/", $_POST['telephone'][2])) {
+if (preg_match("/[^0-9]/", $_POST['telephone_last'])) {
     $error['telephone'] = 'match';
 }
 ?>
